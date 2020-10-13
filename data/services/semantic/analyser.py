c5b13434c3cb82d1213e6452637c504d49678c26
oumaima ben khelifa <oumaima.benkhelifa@clevertech-france.fr>
2020-10-13 17:21:09 +0100

    count the percentage of the semantically valid and invalid dominant subcategory

--- data/services/semantic/analyser.py
+++ data/services/semantic/analyser.py
@@ -3,7 +3,7 @@ from data.models.basic_models import SyntacticResult, SemanticResult, SemanticDa
 from data.models import MATCHED_EXPRESSIONS, DATA_TYPES, COLUMN_TYPE
 from data.services.syntactic.string import StringAnalyser
 import numpy as np
-from data.models import M101_1, M102_2, M103_3, M104_4
+from data.models import M101_1, M102_2, M103_3, M104_4, M105_5, M106_6
 
 
 class SemanticAnalyser(SemanticInterface):
@@ -34,12 +34,14 @@ class SemanticAnalyser(SemanticInterface):
         SemanticResult.objects.update_or_create(document_id=self.document_id, rule=M102_2,
                                                 defaults={'result': {i: res_subcat[columns.get_loc(i)] for i in columns}})
 
-    def count_percentage_of_semantically_valid_values_according_to_the_dominant_category(self):
-        """ Count the percentage of the semantically valid and invalid dominant category"""
+    def count_validation_percentages(self):
+        """ Count the percentage of the semantically valid and invalid to the dominant category and subcategory"""
         qs = SyntacticResult.objects.get(document_id=self.document_id, rule=COLUMN_TYPE)
         data_type = SemanticData.objects.get(document_id=self.document_id)
         nbr_valid_cat = []
+        nbr_valid_subcat = []
         for i in qs.result:
+            subcat = qs.result[i][0]
             if data_type.data[i] == 'datadict':
                 qs_res = SyntacticResult.objects.get(document_id=self.document_id, rule=DATA_TYPES)
             else:
@@ -49,12 +51,19 @@ class SemanticAnalyser(SemanticInterface):
                 if j[0] == qs.result[i][0][0]:
                     res_cat += qs_res.result[i][1][qs_res.result[i][0].index(j)]
             nbr_valid_cat.append(res_cat)
+            nbr_valid_subcat.append(qs_res.result[i][1][qs_res.result[i][0].index(subcat)])
         SemanticResult.objects.update_or_create(document_id=self.document_id, rule=M103_3,
                                                 defaults={'result': {i: nbr_valid_cat[self.df.columns.get_loc(i)] for i in self.df.columns}})
+        SemanticResult.objects.update_or_create(document_id=self.document_id, rule=M105_5,
+                                                defaults={
+                                                    'result': {i: nbr_valid_subcat[self.df.columns.get_loc(i)] for i in
+                                                               self.df.columns}})
+
         nbr_invalid_cat = [100 - num for num in nbr_valid_cat]
+        nbr_invalid_subcat = [100 - num for num in nbr_valid_subcat]
         SemanticResult.objects.update_or_create(document_id=self.document_id, rule=M104_4,
                                                 defaults={'result': {i: nbr_invalid_cat[self.df.columns.get_loc(i)] for i in self.df.columns}})
-
-
-
-
+        SemanticResult.objects.update_or_create(document_id=self.document_id, rule=M106_6,
+                                                defaults={
+                                                    'result': {i: nbr_invalid_subcat[self.df.columns.get_loc(i)] for i in
+                                                               self.df.columns}})
--- data/services/semantic/interfaces.py
+++ data/services/semantic/interfaces.py
@@ -6,15 +6,9 @@ class SemanticInterface:
         """
         pass
 
-    def count_percentage_of_semantically_valid_values_according_to_the_dominant_category(self):
+    def count_validation_percentages(self):
         """
-        Indicator of percentage of semantically valid (and invalid) values according to the dominant category
-        """
-        pass
-
-    def count_percentage_of_semantically_valid_values_according_to_the_dominant_subcategory(self):
-        """
-        Indicator of percentage of semantically valid (and invalid) values according to the dominant subcategory
+        Indicator of percentage of semantically valid (and invalid) values according to the dominant
+        category and subcategory.
         """
         pass
-
