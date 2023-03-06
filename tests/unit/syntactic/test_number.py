4c835b1bb4ebe86f1211e31f84dc5c489d6bd14e
Ayed-Nejmeddine <nejmeddine.ayed@clevertech-france.fr>
2023-03-06 11:42:17 +0100

    # fix test architecture

--- tests/unit/syntactic/test_number.py
+++ tests/unit/syntactic/test_number.py
@@ -1,9 +1,11 @@
+"""Here test number"""
 import numpy as np
 import pytest
 
 
 @pytest.mark.django_db
 def test_compute_min_value(_number_document_fixture):
+    """Test compute min value."""
     # Call the compute_min_value() function and get the actual result
     result = _number_document_fixture.compute_min_value()
     # Define the expected result
@@ -15,6 +17,7 @@ def test_compute_min_value(_number_document_fixture):
 
 @pytest.mark.django_db
 def test_compute_max_value(_number_document_fixture):
+    """Test compute max value."""
     # Call the compute_max_value() function and get the actual result
     result = _number_document_fixture.compute_max_value()
     # convert expected result to the same data type as the result
@@ -25,6 +28,7 @@ def test_compute_max_value(_number_document_fixture):
 
 @pytest.mark.django_db
 def test_compute_average_value(_number_document_fixture):
+    """Test compute average value."""
     # Call the compute_average_value() function and get the actual result
     result = _number_document_fixture.compute_average_value()
     # convert expected result to the same data type as the result
@@ -35,6 +39,7 @@ def test_compute_average_value(_number_document_fixture):
 
 @pytest.mark.django_db
 def test_compute_mode_value(_number_document_fixture):
+    """Test compute mode value."""
     # Call the compute_mode_value() function and get the actual result
     result = _number_document_fixture.compute_mode_value()
     # convert expected result to the same data type as the result
@@ -45,6 +50,7 @@ def test_compute_mode_value(_number_document_fixture):
 
 @pytest.mark.django_db
 def test_compute_median_value(_number_document_fixture):
+    """Test compute median value."""
     # Call the compute_median_value() function and get the actual result
     result = _number_document_fixture.compute_median_value()
     # convert expected result to the same data type as the result
@@ -55,6 +61,7 @@ def test_compute_median_value(_number_document_fixture):
 
 @pytest.mark.django_db
 def test_count_values_number(_number_document_fixture):
+    """Test count values number."""
     # Call the count_values() function and get the actual result
     result = _number_document_fixture.count_values()
     # Define the expected result
