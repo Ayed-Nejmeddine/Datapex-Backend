8b3882ad630b878afa7379c7cfc98d89105373c8
Hnia Mohamed Aymen <mohamedaymen.hnia@clevertech-france.fr>
2023-05-05 12:56:35 +0100

    #Profile photo

--- data/urls.py
+++ data/urls.py
@@ -4,12 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from data.controllers.user import VerificationViewSet
 from data.controllers.city import CityViewSet, CountryViewSet
-
+from data.controllers.user import UploadPhotoViewSet
 
 ROUTER = routers.DefaultRouter()
 ROUTER.register(r'upload-document', DocumentViewSet)
 ROUTER.register(r'phone', VerificationViewSet, basename='phone')
 ROUTER.register(r'cities', CityViewSet)
 ROUTER.register(r'countries', CountryViewSet)
+ROUTER.register(r'photo', UploadPhotoViewSet, basename='photo')
+
 
 urlpatterns = ROUTER.urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
