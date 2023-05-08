12fc0db57b471eef1b89af56417b0c45298e4b19
Ayed-Nejmeddine <131363393+Ayed-Nejmeddine@users.noreply.github.com>
2023-05-08 17:44:45 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 add-apt-repository ppa:ubuntugis/ppa
 apt-get update
 apt-get -y install gdal-bin libgdal-dev
-gunicorn --bind=0.0.0.0 --timeout 600 data_appraisal.wsgi
 echo "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 ls
+gunicorn --bind=0.0.0.0 --timeout 600 data_appraisal.wsgi
