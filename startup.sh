35b25bda89e84fb78c8b7d69cbb16053a71c2551
Ayed-Nejmeddine <131363393+Ayed-Nejmeddine@users.noreply.github.com>
2023-05-09 09:37:01 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,4 @@
 add-apt-repository ppa:ubuntugis/ppa
 apt-get update
 apt-get -y install gdal-bin libgdal-dev
-
-source antenv/bin/activate
-python manage.py makemigrations
-python manage.py makemigrations data
-python manage.py migrate
-
 gunicorn --bind=0.0.0.0 --timeout 600 data_appraisal.wsgi
