2a984b91896a65560fd13a54f221498166d7625b
Ayed-Nejmeddine <nejmeddine.ayed@clevertech-france.fr>
2023-05-10 12:41:27 +0100

    # update settings

--- data_appraisal/settings.py
+++ data_appraisal/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "k%w=%+0%hzi4qy6c2g_5n_g7nqrho3=jlvucv!3%766%z*pelk"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
