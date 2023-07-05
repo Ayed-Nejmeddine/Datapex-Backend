5560891e60df84ad32fb8e687aacd027eaaccd5c
ChirazBelaalia <chiraz.belaalia@clevertech-france.fr>
2023-07-05 10:55:51 +0100

    add filter branch setting

--- data_appraisal/settings.py
+++ data_appraisal/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
 from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -29,6 +31,7 @@ DEBUG = config('DEBUG',default=False,cast=bool)
 ALLOWED_HOSTS = ["*"]
 
 
+os.environ['FILTER_BRANCH_SQUELCH_WARNING'] = '1'
 # Application definition
 
 INSTALLED_APPS = [
